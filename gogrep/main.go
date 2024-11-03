a87eb1d261501cd29ea712eecb7b11963595382a
andyaspel <aspelandy@outlook.com>
2024-11-03 15:38:27 +0100

    added logic to main file

--- gogrep/main.go
+++ gogrep/main.go
@@ -1,7 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/alexflint/go-arg"
+	"github.com/andyaspel/gogrep/worker"
+	"github.com/andyaspel/gogrep/worklist"
+)
+
+func discoverDirs(wl *worklist.Worklist, path string) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("Read-dir Error:", err)
+		return
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			nextPath := filepath.Join(path, entry.Name())
+			discoverDirs(wl, nextPath)
+		} else {
+			wl.Add(worklist.NewJob(filepath.Join(path, entry.Name())))
+		}
+	}
+}
+
+var args struct {
+	SearchTerm string `arg:"positional, required"`
+	SearchDir  string `arg:"positional"`
+}
 
 func main() {
-	fmt.Println("BOLLOCKS!")
+	fmt.Printf("\n\tBOLLOCKS!\n")
+	arg.MustParse(&args)
+
+	var workersWg sync.WaitGroup
+	wl := worklist.New(100)
+	results := make(chan worker.Result, 100)
+	nrWorkers := 10
+	workersWg.Add(1)
+
+	go func() {
+		defer workersWg.Done()
+		discoverDirs(&wl, args.SearchDir)
+		wl.Finalize(nrWorkers)
+	}()
+	for i := 0; i < nrWorkers; i++ {
+		workersWg.Add(1)
+		go func() {
+			defer workersWg.Done()
+			for {
+				workEntry := wl.Next()
+				if workEntry.Path != "" {
+					workerResult := worker.FindInFile(workEntry.Path, args.SearchTerm)
+					if workerResult != nil {
+						for _, r := range workerResult.Inner {
+							results <- r
+						}
+					}
+				} else {
+					return
+				}
+			}
+		}()
+	}
+	blockWorkersWg := make(chan struct{})
+	go func() {
+		workersWg.Wait()
+		close(blockWorkersWg)
+	}()
+
+	var displayWg sync.WaitGroup
+	displayWg.Add(1)
+	go func() {
+		for {
+			select {
+			case r := <-results:
+				fmt.Printf("\t%v[%v]:%v\n", r.Path, r.LineNr, r.Line)
+			case <-blockWorkersWg:
+				if len(results) == 0 {
+					displayWg.Done()
+					return
+				}
+
+			}
+		}
+	}()
+	displayWg.Wait()
 }
